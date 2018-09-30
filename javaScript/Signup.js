c28d509838a817a1fc2c754c538b11065666f1ef
I Burger <Easy@Is-MacBook-Air.local>
2018-09-30 12:30:31 +0200

    initial commit

--- javaScript/Signup.js
+++ javaScript/Signup.js
@@ -8,7 +8,7 @@ signup.onclick = function(){
         var newPassword = document.getElementById("newPassword").value;
         var repeatPassword = document.getElementById("repeatPassword").value;
 
-// CHECK HOW DO I CHECK / REFER TO LIST IN LogIn.js ???
+// CHECK: HOW DO I CHECK / REFER TO LIST IN LogIn.js ???
 
 for (let i = 0;i < userList.length; i++) {
     if (newUsername == userList[i].username) {
@@ -17,7 +17,9 @@ for (let i = 0;i < userList.length; i++) {
 }
 if (newPassword === repeatPassword && newPassword != "") {
     alert ("You are signed up now"); userList.push({username: newUsername, password: newPassword})
-} else {
+} 
+//CHECK: How do I implement a button in You are signed up alert saying " Go to
+else {
     // TO BE IMPLEMENTED: can we make a red alert saying " password input doesnt match"
     alert ("Please enter password or repeat password");
     }
