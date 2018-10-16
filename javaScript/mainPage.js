a7d2d6e1cfe7422ee9c9c110f30e3849f700811a
I Burger <Easy@Is-MacBook-Air.local>
2018-10-16 11:18:32 +0200

    implemented map and table for fleamarkets, added logout and wishlist button

---
+++ javaScript/mainPage.js
@@ -0,0 +1,34 @@
+var wishlist = document.getElementById('wishlist');
+
+wishlist.onclick = function() {
+    document.location.href = "Wishlist.html"; 
+} 
+
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 55.676098, lng: 12.568337},
+    zoom: 8
+    });
+}
+
+function myFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
