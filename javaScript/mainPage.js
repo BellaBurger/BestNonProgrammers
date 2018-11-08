b085da7dd50fb2390976a4bf028ab92f63ca07d8
I Burger <Easy@Is-MacBook-Air.local>
2018-11-08 12:41:23 +0100

    created marker class to automatically create marker on maps, button value needs to be fixed

--- javaScript/mainPage.js
+++ javaScript/mainPage.js
@@ -3,7 +3,6 @@ var userID = JSON.parse(localStorage.getItem('currentUser'));
 console.log(userID);
 
 // Help from Henrik - Fleamarket created as class
-//var display
 class Fleamarket {
     constructor(userID, name, location, lat, lng, date, image){
         this.id = userID;
@@ -56,12 +55,6 @@ class Fleamarket {
         //if fleamarket is in storage this.button = document.getElementIdby("Added to Wishlist")
         return "<tr><td>"+ this.name + "</td><td>" + this.location + "</td><td>" + this.date + "</td><td>" + this.button + "</td></tr>";
     }
-
-
-// This function creats a marker in te map for each fleamarket
-    createMarker (){
-        return new google.maps.Marker({position: this.lat + "," + this.lng, map: map})
-    }
 }
 
 
@@ -71,6 +64,7 @@ list.push(new Fleamarket(userID, "Flmrkt1", "Nørrebro", 55.6918268, 12.54927120
 list.push(new Fleamarket(userID, "Flmrkt2", "Sydhavn", 55.654884, 12.537608, new Date(2018, 11, 24, 10, 33, 30, 0), null));
 list.push(new Fleamarket(userID, "Flmrkt3", "Frederiksberg", 55.675378, 12.528474, new Date(2018, 11, 24, 10, 33, 30, 0), null));
 list.push(new Fleamarket(userID, "Flmrkt4", "Valby", 55.666290, 12.514340, new Date(2018, 11, 24, 10, 33, 30, 0), null));
+console.log(list)
 //for every object in the array the function creatHTML is called
 var html = "";
 //when button is clicked add event to userEvents and store in local storage
@@ -91,20 +85,24 @@ var buttons = document.getElementsByClassName('addToList');
 // Get the list of wishes from localstorage and parse it from json to array. If local Storage is empty use empty array, 
 var wishes;
 if (localStorage.getItem('wishes') == null) { 
-wishes = []
-} else { wishes = JSON.parse(localStorage.getItem('wishes'))
+    wishes = []
+    } else { 
+    wishes = JSON.parse(localStorage.getItem('wishes'))
+} 
+console.log(wishes)
+
+//TBD disable button if fleamarket already in local storage
+for(s=0; s<list.length; s++){
+    for(t=0; t < wishes.length; t++) {
+        if (list[s].name == wishes[t].name && wishes[t].id.userName == userID[0].userName){
+            document.getElementById("click").disabled = true;
+           
+        }
+    }
 }
+console.log("is it working")
 
-
-
-// Creat function that checks if item is already in local storage
-// if function = true {alert = already in wishlist}
-// else {for (U=0.....)}
-
-
-
-//if (list[i] == storedWishes[i]){
-  //   alert ("already in wishlist") 
+//Alert ("already in wishlist") if fleamarket stored in local storage under the current user's user ID
 for(u=0; u < buttons.length; u++){
     buttons[u].addEventListener('click', function(e){
         console.log(this);
@@ -115,11 +113,22 @@ for(u=0; u < buttons.length; u++){
             } 
         }
 
+        /*for(u=0; u < buttons.length; u++){
+            for (j=0; j< wishes.length; j++) {
+                if (JSON.parse(this.dataset.object).name == wishes[j].name && userID[0].userName == wishes[j].id[0].userName){
+                    document.getElementById("click").value = "added to Wishlist"
+                    document.getElementById("added to Wishlist").disabled = true;
+                    return 
+                }
+            } 
+        }*/
+
         // Push wish to array and change button value
         wishes.push(JSON.parse(this.dataset.object));
         var listString = JSON.stringify(wishes);
         localStorage.setItem('wishes', listString);
         
+       
        //document.getElementById("click").value = "added to Wishlist";
 
         // Save list to localstorage, but remember to parse it to json first
@@ -129,11 +138,6 @@ for(u=0; u < buttons.length; u++){
      //   alert("CLEVER");
     //});
 }
-
-
-
-
-
 
 var wishlist = document.getElementById('wishlist');
 wishlist.onclick = function() {
@@ -145,47 +149,40 @@ logout.onclick = function() {
     document.location.href = 'LogIn.html'; 
 }
 // Create a loop to retrieve lat and lng (coordinates) from every fleamarket
+
+// Create markers for all fleamarkets
+var markers = [];
+class marker {
+    constructor(name, lat, lng){
+        this.name = name;
+        this.lat = lat;
+        this.lng = lng;
+    }
+}
+// function to create variable that includes information for map 
+var createMarker = function(){
+    markers.push(new marker(list[m].name, list[m].lat, list[m].lng)) 
+    } 
+for (m=0; m<list.length; m++ ){
+    createMarker() 
+    } 
+console.log(markers);
+
+//initialize map
 var map;
+var cph = {lat: 55.676098, lng: 12.568337}
 function initMap() {
-    var cph = {lat: 55.676098, lng: 12.568337}
-    var nrbr = {lat: 55.6918268, lng: 12.549271207226749}
-    var shvn = {lat: 55.654884, lng: 12.537608}
-    var fbrg = {lat:55.675378, lng: 12.528474}
-    var vlb = {lat: 55.666290, lng: 12.514340}
     map = new google.maps.Map(document.getElementById('map'), {
     center: cph,
     zoom: 8
     });
-    var marker1 = new google.maps.Marker({position: nrbr, map: map});
-    var marker2 = new google.maps.Marker({position: shvn, map: map});
-    var marker3 = new google.maps.Marker({position: fbrg, map: map});
-    var marker4 = new google.maps.Marker({position: vlb, map: map});
-    //return displayMarkers
-}
-    /*
-    // Create an array of alphabetical characters used to label the markers.
-    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-    // Add some markers to the map.
-    // Note: The code uses the JavaScript Array.prototype.map() method to
-    // create an array of markers based on a given "locations" array.
-    // The map() method here has nothing to do with the Google Maps API.
-    var markers = list.map(function(location, i) {
-      return new google.maps.Marker({
-        position: location,
-        label: labels[i % labels.length]
-      });
-    });
-}
-
-  var displayMarkers = function(){
-      for(y=0; y < list.length; y++ ){
-     list[y].createMarker();
+    //Loop to display markers on map
+    for (n=0; n < markers.length; n++){
+        new google.maps.Marker({position: {lat: markers[n].lat, lng: markers[n].lng},map: map})
     }
-  }
-console.log()
-*/
+}
 
+//Implement search function that loops through table rows
 function myFunction() {
     // Declare variables
     var input, filter, table, tr, td, i;
